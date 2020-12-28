a69d7f35ebfb0def72eb0a0cd354c89725d7f074
whitequark <whitequark@whitequark.org>
2020-12-28 08:03:10 +0000

    Fix caching so that it actually checks WASM binary digest.
    Embarrassing.

--- pypi-ecp5/yowasp_nextpnr_ecp5/__init__.py
+++ pypi-ecp5/yowasp_nextpnr_ecp5/__init__.py
@@ -1,6 +1,7 @@
 import sys
 import wasmtime
 import pathlib
+import hashlib
 import appdirs
 try:
     from importlib import resources as importlib_resources
@@ -13,6 +14,9 @@ except ImportError:
 
 
 def _run_wasm_app(wasm_filename, argv):
+    module_binary = importlib_resources.read_binary(__package__, wasm_filename)
+    module_digest = hashlib.sha1(module_binary).digest()
+
     wasi_cfg = wasmtime.WasiConfig()
     wasi_cfg.argv = argv
     wasi_cfg.preopen_dir(str(importlib_resources.files(__package__) / "share"), "/share")
@@ -26,15 +30,20 @@ def _run_wasm_app(wasm_filename, argv):
     cache_path = pathlib.Path(appdirs.user_cache_dir("yowasp"))
     cache_path.mkdir(parents=True, exist_ok=True)
     cache_filename = (cache_path / "{}-cache".format(wasm_filename))
+    digest_filename = (cache_path / "{}-digest".format(wasm_filename))
     try:
+        with digest_filename.open("rb") as digest_file:
+            if digest_file.read() != module_digest:
+                raise Exception("cache miss")
         with cache_filename.open("rb") as cache_file:
             module = wasmtime.Module.deserialize(engine, cache_file.read())
     except:
         print("Preparing to run {}. This might take a while...".format(argv[0]), file=sys.stderr)
-        module = wasmtime.Module(engine,
-            importlib_resources.read_binary(__package__, wasm_filename))
+        module = wasmtime.Module(engine, module_binary)
         with cache_filename.open("wb") as cache_file:
             cache_file.write(module.serialize())
+        with digest_filename.open("wb") as digest_file:
+            digest_file.write(module_digest)
 
     store = wasmtime.Store(engine)
     linker = wasmtime.Linker(store)
