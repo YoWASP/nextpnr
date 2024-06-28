6cfb3b31ddcd1d562303760c419f2e41d76a4aa6
Catherine <whitequark@whitequark.org>
2024-06-28 15:44:03 +0000

    [skip py] Update `api.d.ts`.

--- npmjs-ice40/lib/api.d.ts
+++ npmjs-ice40/lib/api.d.ts
@@ -2,20 +2,29 @@ export type Tree = {
     [name: string]: Tree | string | Uint8Array
 };
 
+export type InputStream =
+    (byteLength: number) => Uint8Array | null;
+
 export type OutputStream =
     (bytes: Uint8Array | null) => void;
+
+export type RunOptions = {
+    stdin?:  InputStream  | null;
+    stdout?: OutputStream | null;
+    stderr?: OutputStream | null;
+    decodeASCII?: boolean;
+    synchronously?: boolean;
+};
+
+export type Command =
+    (args?: string[], files?: Tree, options?: RunOptions) => Promise<Tree> | Tree | undefined;
 
 export class Exit extends Error {
     code: number;
     files: Tree;
 }
 
-export type Command = (args?: string[], files?: Tree, options?: {
-    stdout?: OutputStream | null,
-    stderr?: OutputStream | null,
-    decodeASCII?: boolean
-}) => Promise<Tree>;
-
+//--------8<--------8<--------8<--------8<--------8<--------8<--------8<--------8<--------8<--------
 
 export const runIcepll: Command;
 export const runIcebram: Command;
